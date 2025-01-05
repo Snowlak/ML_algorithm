103c71babe23cc3a32191fdb7a5d74a4ac1812a6
Snowlak <cnejaapolonik@gmail.com>
2025-01-05 22:31:10 +0300

    added regulation parameter

--- mylinereg.py
+++ mylinereg.py
@@ -3,12 +3,15 @@ import numpy as np
 
 
 class MyLineReg:
-    def __init__(self, n_iter: int = 100, learning_rate: float = 0.1, metric=None):
+    def __init__(self, n_iter: int = 100, learning_rate: float = 0.1, metric=None,
+                 reg: str = None, l1_coef=0, l2_coef=0):
         self.iter = n_iter
         self.learning_rate = learning_rate
         self.weights = []
         self.metric = metric
         self.loss = 0
+        self.reg = reg
+        self.reg_coef = (l1_coef, l2_coef)
 
     def __str__(self):
         return f'MyLineReg class: n_iter={self.iter}, learning_rate={self.learning_rate}'
@@ -17,11 +20,11 @@ class MyLineReg:
         one_column = np.ones(len(x))
         x.insert(0, 'x0', one_column)
         self.weights = np.ones(x.shape[1])
-        y_prediction = pd.Series()
         for i in range(self.iter):
             y_prediction = x.dot(self.weights)
             self.loss = self.metric_error(y, y_prediction)
-            grad = (2/len(y)) * np.dot(x.T, (y_prediction - y))
+            grad_reg = self.grad_reg()
+            grad = (2/len(y)) * np.dot((y_prediction - y), x) + grad_reg
             self.weights = self.weights - grad * self.learning_rate
             if verbose:
                 if i == 0:
@@ -29,6 +32,16 @@ class MyLineReg:
                 elif i % verbose == 0:
                     print(f'{i} | loss: {self.loss}|{self.metric}: {self.loss}')
         self.loss = self.metric_error(y, x.dot(self.weights))
+
+    def grad_reg(self):
+        l1 = self.reg_coef[0] * np.sign(self.weights)
+        l2 = self.reg_coef[1] * 2 * self.weights
+        if self.reg == 'l1':
+            return l1
+        elif self.reg == 'l2':
+            return l2
+        elif self.reg == 'elasticnet':
+            return l1 + l2
 
     def metric_error(self, y: pd.Series, y_prediction: pd.Series):
         if self.metric == 'mae':
@@ -46,7 +59,7 @@ class MyLineReg:
         return self.loss
 
     def get_coef(self):
-        return np.mean(self.weights[1::])
+        return self.weights[1::]
 
     def predict(self, x: pd.DataFrame):
         one_column = np.ones(len(x))
